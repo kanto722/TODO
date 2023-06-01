d129d4f165ce8f0f1d54fac73a6eadf4ca99e90b
Kanto72 <kanto72@yandex.ru>
2023-06-01 22:55:32 +0300

    vuetify 3

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    
+    <HelloWorld />
   </div>
 </template>
 
@@ -10,7 +10,7 @@ import { defineComponent } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
-  name: 'HomeView',
+  name: 'TodoView',
   components: {
     HelloWorld,
   },
